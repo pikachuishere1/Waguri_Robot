87ee60c0e241aa1009fb3133088a3eb8f98fb242
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-18 21:28:32 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_SUPPORT",
+            text="˹ 𝐖⋏ɢ𝛖ʀ𝛊 ꭙ 𝐑σвσт ˼",
+            url=f"https://t.me/Waguri_Robot",
         ),
     ],
 ]
